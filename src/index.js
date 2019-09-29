030b620d46c872220eb6ab401ca8d43490e44f4e
fainz777 <fainz777@gmail.com>
2019-09-29 19:29:42 +0300

    brackets/feature: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let temp = '';
+
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (bracketsConfig[j][0] === str[i]) {
+        if (temp.length && temp[temp.length - 1] === bracketsConfig[j][0] && bracketsConfig[j][1] === str[i]) {
+          temp = temp.slice(0, temp.length - 1);
+        } else {
+          temp += str[i];
+        }
+        
+      } else if (bracketsConfig[j][1] === str[i] ) {
+         if (temp[temp.length - 1] === bracketsConfig[j][0]) {
+           temp = temp.slice(0, temp.length - 1);
+         } else if (temp[temp.length - 1] !== bracketsConfig[j][0]) {
+           return false;
+         }
+      }
+    }
+  }
+
+  if (temp.length) {
+    return false;
+  }
+
+  return true;
 }
+
+
